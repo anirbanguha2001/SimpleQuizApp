b4e8fd7e32144b821eb93feb1b88bdb3c55c20b6
Anirban Guha <91046921+anirbanguha2001@users.noreply.github.com>
2023-10-12 01:11:40 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.programminghut.quizapp;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Which of the following programming languages is primarily used for web development?",
+            "What does CPU stand for in computer terms?",
+            "Which of the following data structures uses LIFO (Last In, First Out) order?",
+            "Which protocol is used for secure communication over a computer network, especially on the internet?",
+            "What does HTML stand for in the context of web development?"
+    };
+    public static String choices[][] = {
+            {"Java","Python","HTML","C++"},
+            {"Central Processing Unit","Central Process Unit","Computer Personal Unit","Central Processor Unit"},
+            {"Queue","Stack","Linked List","Tree"},
+            {"FTP","HTTP","SSL/TLS","DHCP"},
+            {"Hyper Transfer Markup Language","Hyper Text Markup Language","High Tech Machine Language"," Home Tool Markup Language"}
+    };
+    public static String correctAnswer[] ={
+            "HTML",
+            "Central Processing Unit",
+            "Stack",
+            "SSL/TLS",
+            "Hyper Text Markup Language"
+
+    };
+}
